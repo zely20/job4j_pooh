7d4adc2206bea4384f35c74a8308b588f9e22f92
Aliaksandr Zelianko <zely20@mail.ru>
2021-04-01 16:47:21 +0300

    Don't work

--- src/main/java/ru/job4j/pooh/QueueService.java
+++ src/main/java/ru/job4j/pooh/QueueService.java
@@ -1,8 +1,27 @@
 package ru.job4j.pooh;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class QueueService implements Service {
+    //ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> queue = new ConcurrentHashMap<>();
+    private final ConcurrentLinkedQueue<String> queue = new ConcurrentLinkedQueue<>();
     @Override
     public Resp process(Req req) {
+        if (req.method().equals("POST")){
+            post(req);
+            return new Resp(req.text(), 204);
+        }
+        if (req.method().equals("GET")){
+            return get(req);
+        }
         return null;
+    }
+
+    private void post (Req req) {
+        queue.offer(req.text());
+    }
+
+    private Resp get (Req req) {
+        return new Resp(queue.poll(), 200);
     }
 }
--- src/main/java/ru/job4j/pooh/Req.java
+++ src/main/java/ru/job4j/pooh/Req.java
@@ -16,9 +16,12 @@ public class Req {
         String method = s[0];
         String[] s2 = s[1].split("/");
         String mode = s2[1];
-        String[] s3 = s[7].split("\r\n\r\n");
-        String text = s3[1];
-        return new Req(method, mode, text);
+        if(method.equals("POST")) {
+            String[] s3 = s[7].split("\r\n\r\n");
+            String text = s3[1];
+            return new Req(method, mode, text);
+        }
+        return new Req(method, mode, null);
     }
 
     public String method() {
